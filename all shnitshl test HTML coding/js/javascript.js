cd68c236dface5641ed773e4003037c2ee23a4b5
daniel <106866674+shnitshl@users.noreply.github.com>
2022-08-20 19:49:45 +0300

    Add files via upload

---
+++ all shnitshl test HTML coding/js/javascript.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const PI = 3.1415;
+let message = 'hello', age = 14, name = 'הגעת לעמוד הבית'
+
+num1 = 30
+num2 = 14
+
+alert("num1 + num2");
+alert("num1 - num2");
+alert("num1 * num2");
+alert("num1 % num2");
+alert("כול הכבוד על התרגיל המתמתי");
+
+
+alert('message');
